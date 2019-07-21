3d2208dbf7213147d8f8baba64dd948f03839988
Niko <nick.oshinov@gmail.com>
2019-07-21 14:41:15 -0400

    Added some more styling to search bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import { GithubLoginButton } from "react-social-login-buttons";
 import './Navbar.css';
 import {login} from "./helpers";
+import { FaAt } from 'react-icons/fa';
 
 export const NavbarComponent = () => {
   const loggedIn = useSelector( state => state.auth.loggedIn );
   const dispatch = useDispatch();
 
   return  <div className="container navbar">
-    <input className="search"/>
+    <div className="searchBar">
+      <FaAt className="searchBarIcon" size={'1.9rem'} color='#777'/>
+      <input className="search" placeholder='Username'/>
+    </div>
+
 
     {/*Logged out nav bar items*/}
     {!loggedIn && <GithubLoginButton id="githubButton" onClick={()=>login(dispatch)}/>}
