6c2ad08334a582876a3b07a637a81ffe85bbc9e2
Niko <nick.oshinov@gmail.com>
2019-07-23 00:39:42 -0400

    modified render test to work with a provider

--- src/components/NewLink/NewLink.test.js
+++ src/components/NewLink/NewLink.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {configure, shallow} from 'enzyme';
+import {Provider} from "react-redux";
+import Adapter from "enzyme-adapter-react-16/build";
+import configureMockStore from 'redux-mock-store';
 import NewLink from './NewLink';
+
+configure({adapter: new Adapter()});
+
+const mockStore = configureMockStore();
 
 describe('<NewLink />', () => {
   test('renders', () => {
-    const wrapper = shallow(<NewLink />);
+    const wrapper = shallow(
+      <Provider store={mockStore({})}>
+        <NewLink />
+      </Provider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
