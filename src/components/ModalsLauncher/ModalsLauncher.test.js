e05c85aba7a77e72b725747d8bcfef2373f89c98
Niko <nick.oshinov@gmail.com>
2019-07-23 12:47:51 -0400

    fixed crashing test

--- src/components/ModalsLauncher/ModalsLauncher.test.js
+++ src/components/ModalsLauncher/ModalsLauncher.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Adapter from "enzyme-adapter-react-16/build";
+import {Provider} from "react-redux";
 import configureMockStore from 'redux-mock-store';
 import ModalsLauncher from './ModalsLauncher';
-import {Provider} from "react-redux";
 
 configure({adapter: new Adapter()});
 
--- src/components/UserPage/UserPage.test.js
+++ src/components/UserPage/UserPage.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {configure, shallow} from 'enzyme';
+import Adapter from "enzyme-adapter-react-16/build";
 import UserPage from './UserPage';
 
+configure({adapter: new Adapter()});
+
 describe('<UserPage />', () => {
+  let urlParam = { params: {userId: 'testUserId1'}};
   test('renders', () => {
-    const wrapper = shallow(<UserPage />);
+    const wrapper = shallow(<UserPage match={urlParam}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
