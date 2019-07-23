5ce69914191570a77884c3aea7373ce455e3a410
Niko <nick.oshinov@gmail.com>
2019-07-23 14:10:01 -0400

    Pritified code

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -8,9 +8,9 @@ import {Provider} from "react-redux";
 import {FirebaseAuth, FirestoreData} from "../../utility/firebaseFascade";
 import {authActionCreators, authActions} from "../../actions";
 import {mockAuthBuilder} from "../../testAssets/firebaseMocks";
-configure({adapter: new Adapter()});
 
 const mockStore = configureMockStore();
+configure({adapter: new Adapter()});
 
 describe('<Navbar />', () => {
   const initialStoreData = {
@@ -52,6 +52,7 @@ describe('<Navbar />', () => {
 
   test('Github login button is not rendered if user is logged in', () => {
     initialStoreData.auth.loggedIn = true;
+
     const wrapper = mount(
       <Provider store={mockStore(initialStoreData)}>
         <Navbar/>
@@ -63,6 +64,7 @@ describe('<Navbar />', () => {
   test('a logged in user sees the signout button', () => {
     initialStoreData.auth.loggedIn = true;
     initialStoreData.auth.username = 'validUsername';
+
     const wrapper = mount(
       <Provider store={mockStore(initialStoreData)}>
         <Navbar/>
@@ -74,9 +76,10 @@ describe('<Navbar />', () => {
   test('Clicking signout button signs user out', () => {
     initialStoreData.auth.loggedIn = true;
     initialStoreData.auth.username = 'validUsername';
+
     const mockFn = jest.fn();
-    // need to use spyOn because signout is readonly (can't be overwritten with RHS assignment).
     jest.spyOn(helpers, 'signOut').mockImplementation(mockFn);
+
     const wrapper = mount(
       <Provider store={mockStore(initialStoreData)}>
         <Navbar/>
@@ -103,6 +106,7 @@ describe('Navbar helpers', ()=>{
   test('login function calls firebase wrapper for sign in withpopup', ()=>{
     FirebaseAuth.loginWithGithub = jest.fn();
     FirebaseAuth.loginWithGithub.mockReturnValue(new Promise(resolve => resolve()));
+
     expect(FirebaseAuth.loginWithGithub).toBeCalledTimes(0);
     helpers.login(dispatcherPlaceholder);
     expect(FirebaseAuth.loginWithGithub).toBeCalledTimes(1);
@@ -110,6 +114,7 @@ describe('Navbar helpers', ()=>{
 
   test('login function calls dispatch 3 times', async ()=>{
     const mock = jest.fn();
+
     expect(mock).toBeCalledTimes(0);
     await helpers.login(mock);
     expect(mock).toBeCalledTimes(3);
@@ -129,6 +134,7 @@ describe('Navbar helpers', ()=>{
 
   test('login function fetches user data', async () => {
     FirestoreData.fetchOwnUserModel = jest.fn(()=>new Promise(resolve=>resolve({username: 'myusername'})));
+
     expect(FirestoreData.fetchOwnUserModel).toBeCalledTimes(0);
     await helpers.login(dispatcherPlaceholder);
     expect(FirestoreData.fetchOwnUserModel).toBeCalledTimes(1);
@@ -137,12 +143,14 @@ describe('Navbar helpers', ()=>{
   test('dispatches an update username action', async () => {
     const model = {username: 'myusername'}
     FirestoreData.fetchOwnUserModel = jest.fn(()=>new Promise(resolve=>resolve({username: model})));
+
     await helpers.login(dispatcherPlaceholder);
     expect(dispatcherPlaceholder).toBeCalledWith(authActionCreators.finishLogin(model));
   });
 
   test('signout calls signout bind of firebaseFascade', async () => {
     FirebaseAuth.signOut = jest.fn();
+
     expect(FirebaseAuth.signOut).toBeCalledTimes(0);
     await helpers.signOut(dispatcherPlaceholder);
     expect(FirebaseAuth.signOut).toBeCalledTimes(1);
@@ -151,6 +159,7 @@ describe('Navbar helpers', ()=>{
   test('signout dispatches signout action', async () => {
     FirebaseAuth.signOut = jest.fn();
     dispatcherPlaceholder = jest.fn();
+
     expect(dispatcherPlaceholder).toBeCalledTimes(0);
     await helpers.signOut(dispatcherPlaceholder);
     expect(dispatcherPlaceholder).toBeCalledTimes(1);
