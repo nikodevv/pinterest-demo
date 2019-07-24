2434c8374b2ae392f6e2610a56cfc245791156fe
Niko <nick.oshinov@gmail.com>
2019-07-24 11:32:05 -0400

    new link button only shows if you are logged in

--- src/components/UserButtons/UserButtons.js
+++ src/components/UserButtons/UserButtons.js
@@ -3,15 +3,19 @@ import './UserButtons.css';
 import { FaRegQuestionCircle, FaPlusCircle} from 'react-icons/fa';
 import * as redux from "react-redux";
 import {modalActionCreators} from "../../actions";
+import {useSelector} from "react-redux";
 
 export const UserButtons = () => {
+  const loggedIn = useSelector(state=>state.auth.loggedIn);
   const dispatch = redux.useDispatch();
   return <div className="userButtonsContainer">
-    <FaPlusCircle id="addLink"
+    { loggedIn === true &&
+     <FaPlusCircle id="addLink"
                   size="2rem"
                   onClick={()=>dispatch(modalActionCreators.toggleNewLinkModal())}
                   className="actionIcon"
     />
+    }
     <a href='https://github.com/nikodevv/pinterest-demo' target={'_blank'}>
       <FaRegQuestionCircle size="2rem"
                            color="#333"
